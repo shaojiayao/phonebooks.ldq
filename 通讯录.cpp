2cb8593caf13afd7d71c61707a05379ce7ebdd9f
shaojiayao <179233648@qq.com>
2025-09-07 16:17:43 +0800

    添加 修改删除功能

--- 通讯录.cpp
+++ 通讯录.cpp
@@ -39,17 +39,9 @@ void addperson(phonebook*pb){
 	
 	cout<<"请输入名字："<<endl;
 	cin>>pb->personArray[pb->m_size].m_name;
-
+	
 	cout<<"请输入年龄："<<endl;
-	int age=0;
-	while(true){
-		cin>>age;
-		if(0<=age&&age<=150){
-			pb->personArray[pb->m_size].m_age=age;
-			break;
-		}
-		cout<<"输入有误，请重新输入"<<endl;
-	}
+	cin>>pb->personArray[pb->m_size].m_age;
 	
 	cout<<"请输入性别："<<endl;
 	cout<<"1---男"<<endl;
@@ -72,16 +64,21 @@ void addperson(phonebook*pb){
 	cin>>pb->personArray[pb->m_size].m_addr;
 	
 	pb->m_size++;
+	system("pause");
+	system("cls");
 }
 
 void print (const phonebook*pb)
 {
+	if(pb->m_size==0){
+		cout<<"通讯录为空"<<endl;
+	}
 	for(int i=0;i<pb->m_size;i++){
 		string sex ;
 		if(	pb->personArray[i].m_sex==1){
 			sex="男";
 		}
-		else if(	pb->personArray[i].m_sex==2)
+		else if(pb->personArray[i].m_sex==2)
 		{
 			sex="女";
 		}
@@ -89,7 +86,116 @@ void print (const phonebook*pb)
 		"   性别："<< sex <<"   号码："<<pb->personArray[i].m_phone<<
 		"   地址："<<pb->personArray[i].m_addr<<endl;
 	}
+	
+	system("pause");
+	system("cls");
 }
+
+int IsExist(const phonebook * abs,string name)
+{
+	for(int i=0;i<abs->m_size;i++){
+		if(name==abs->personArray[i].m_name){
+			return i;
+		}
+	}
+	return -1;
+}
+void deleteperson(phonebook * pb)
+{
+	string name;
+	cout<<"请输入名字"<<endl;
+	cin>>name;
+	int ret=IsExist(pb,name);
+	if(ret!=-1){
+		for(int i=ret;i<pb->m_size;i++){
+			pb->personArray[i]=pb->personArray[i+1];
+		}
+		pb->m_size--;
+		cout<<"删除成功"<<endl;
+	}
+	else
+		cout<<"查无此人"<<endl;
+	
+	system("pause");
+	system("cls");
+}
+
+void Findperson(phonebook *pb)
+{
+	string name;
+	cout<<"请输入名字"<<endl;
+	cin>>name;
+	int ret=IsExist(pb,name);
+	if(ret!=-1){
+		string sex ;
+		if(	pb->personArray[ret].m_sex==1){
+			sex="男";
+		}
+		else if(pb->personArray[ret].m_sex==2)
+		{
+			sex="女";
+		}
+		cout<<"名字："<<pb->personArray[ret].m_name<<"\t"
+		<<"   年龄："<<pb->personArray[ret].m_age<<"\t"
+		<<"   性别："<<pb->personArray[ret].m_sex<<"\t"
+		<<"   号码："<<pb->personArray[ret].m_phone<<"\t"
+		<<"   地址："<<pb->personArray[ret].m_addr<<endl;
+	}
+	else
+	{
+		cout<<"查无此人"<<endl;
+	}
+	
+	system("pause");
+	system("cls");
+}
+
+void modifyperson(phonebook*pb)
+{
+	string name;
+	cout<<"请输入修改人名字"<<endl;
+	cin>>name;
+	int ret=IsExist(pb,name);
+	if(ret!=-1){
+		string name;
+		cout<<"请输入修改后名字"<<endl;
+		cin>>name;
+		pb->personArray[ret].m_name=name;
+		
+		cout<<"请输入修改后年龄"<<endl;
+		cin>>pb->personArray[ret].m_age;
+		
+		cout<<"请输入修改后性别"<<endl;
+		cout<<"1---男"<<endl;
+		cout<<"2---女"<<endl;
+		while(true)
+		{
+			int sex;
+			cin>>sex;
+			if(sex==1||sex==2){
+				pb->personArray[pb->m_size].m_sex=sex;
+				break;
+			}
+			cout<<"输入有误，请重新输入"<<endl;
+		}
+		
+		
+		cout<<"请输入修改后电话"<<endl;
+		cin>>pb->personArray[ret].m_phone;
+		
+		cout<<"请输入修改后住址"<<endl;
+		cin>>pb->personArray[ret].m_addr;
+	}
+	else
+	{
+		cout<<"查无此人"<<endl;
+	}
+	
+	system("pause");
+	system("cls");
+	
+}
+
 void loop(){
 	phonebook pb;
 	
@@ -110,13 +216,15 @@ void loop(){
 			print(&pb);
 			break;
 		case 3:
-			cout<<"删除联系人"<<endl;
-			break;
+			{
+				deleteperson(&pb);
+				break;   
+			}
 		case 4:
-			cout<<"查找联系人"<<endl;
+			Findperson(&pb);
 			break;
 		case 5:
-			cout<<"修改联系人"<<endl;
+			modifyperson(&pb);
 			break;
 		case 6:
 			cout<<"清空联系人"<<endl;
@@ -135,12 +243,8 @@ void loop(){
 
 
 int main(){
-
+	
 	loop();
-
-	
-	
-	
 	
 	//system("pause");
 	return 0;
